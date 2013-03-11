5e7d8dcc61afdf689e1d17989dee3672802303ed
Scott Hendrickson <shendrickson@shendrickson6.gnip.com>
2013-03-11 16:02:11 +0000

    Rolling logs

--- src/GnipStreamCollectorMetrics.py
+++ src/GnipStreamCollectorMetrics.py
@@ -20,13 +20,13 @@ from Latency import Latency
 CHUNK_SIZE = 2**16        # decrease for v. low volume streams, > max record size
 GNIP_KEEP_ALIVE = 30      # 30 sec gnip timeout
 MAX_BUF_SIZE = 2**22      # bytes records to hold in memory
-MAX_ROLL_SIZE = 2**32     # force time-period to roll forward 
+MAX_ROLL_SIZE = 2**30     # force time-period to roll forward 
 NEW_LINE = '\r\n'
 
 class GnipStreamClient(object):
     def __init__(self, _streamURL, _streamName, _userName, _password,
             _filePath, _rollDuration, _procThread):
-        logging.info('GnipStreamClient started')
+        logr.info('GnipStreamClient started')
         self.rollDuration = _rollDuration
         self.streamName = _streamName
         self.streamURL = _streamURL
@@ -45,9 +45,9 @@ class GnipStreamClient(object):
         while True:
             try:
                 self.getStream()
-                logging.error("Forced disconnect: %s"%e)
+                logr.error("Forced disconnect: %s"%e)
             except ssl.SSLError, e:
-                logging.error("Connection failed: %s"%e)
+                logr.error("Connection failed: %s"%e)
 
     def getStream(self):
         req = urllib2.Request(self.streamURL, headers=self.headers)
@@ -67,11 +67,11 @@ class GnipStreamClient(object):
                 try:
                     [records, self.string_buffer] = self.string_buffer.rsplit(NEW_LINE,1)
                     timeSpan = test_time - self.time_roll_start
-                    logging.debug("recsize=%d, %s, %s, ts=%d, dur=%d"%
+                    logr.debug("recsize=%d, %s, %s, ts=%d, dur=%d"%
                             (len(records), self.streamName, self.filePath, 
                                 test_time, timeSpan))
                     self.procThreadObj(records, self.streamName, self.filePath,
-                            logging, self.time_roll_start, timeSpan).start()
+                            logr, self.time_roll_start, timeSpan).start()
                     if self.rollForward(test_time, test_size):
                         self.time_roll_start = test_time
                         roll_size = 0
@@ -83,11 +83,11 @@ class GnipStreamClient(object):
     def rollForward(self, ttime, tsize):
         # these trigger both processing and roll forward
         if ttime - self.time_roll_start >= self.rollDuration:
-            logging.debug("Roll: duration (%d>=%d)"%
+            logr.debug("Roll: duration (%d>=%d)"%
                     (ttime-self.time_roll_start, self.rollDuration))
             return True
         if tsize >= MAX_ROLL_SIZE:
-            logging.debug("Roll: size (%d>=%d)"%
+            logr.debug("Roll: size (%d>=%d)"%
                     (tsize, MAX_ROLL_SIZE))
             return True
         return False
@@ -98,7 +98,7 @@ class GnipStreamClient(object):
         if NEW_LINE not in self.string_buffer:
             return False
         if len(self.string_buffer) > MAX_BUF_SIZE:
-            logging.debug("Triggered: buffer size (%d>%d)"%
+            logr.debug("Trigger: buffer size (%d>%d)"%
                     (len(self.string_buffer), MAX_BUF_SIZE))
             return True
         return self.rollForward(ttime, tsize)
@@ -106,13 +106,15 @@ class GnipStreamClient(object):
 if __name__ == '__main__':
     config = ConfigParser.ConfigParser()
     config.read('gnip.cfg')
-    # logging  level=logging.DEBUG for lots of detail
+    # logger 
     logfilepath = config.get('sys','logfilepath')
-    logging.basicConfig(filename=logfilepath + "/gnip-stream-log",
-                    format='%(asctime)s %(levelname)s %(funcName)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    #level=logging.ERROR)
-                    level = logging.DEBUG)
+    logr = logging.getLogger('GnipStreamLogger')
+    rotating_handler = logging.handlers.RotatingFileHandler(
+            filename=logfilepath + "/gnip-stream-log",
+            mode='a', maxBytes=2**24, backupCount=5)
+    rotating_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(funcName)s %(message)s"))
+    logr.setLevel(logging.DEBUG)
+    logr.addHandler(rotating_handler)
     # set up authentication
     username = config.get('auth','username')
     password = config.get('auth','password')
@@ -120,11 +122,11 @@ if __name__ == '__main__':
     streamurl = config.get('stream', 'streamurl')
     streamname = config.get('stream', 'streamname')
     filepath = config.get('stream', 'filepath')
-    logging.info("Collection starting for stream %s"%(streamurl))
+    logr.info("Collection starting for stream %s"%(streamurl))
     # Determine processing method
     rollduration = int(config.get('proc', 'rollduration'))
     processtype = config.get('proc', 'processtype')
-    logging.info("processing strategy %s"%processtype)
+    logr.info("processing strategy %s"%processtype)
     proc = None
     if processtype == "latency":
         proc = Latency
@@ -135,9 +137,9 @@ if __name__ == '__main__':
     elif processtype == "redis":
         proc = Redis
     else:
-        logging.error("No valid processing strategy selected (%s), aborting"%processtype)
+        logr.error("No valid processing strategy selected (%s), aborting"%processtype)
         sys.exit(-1)
-    logging.info("Storing data in path %s"%(filepath))
+    logr.info("Storing data in path %s"%(filepath))
     client = GnipStreamClient(streamurl, streamname, username, password, 
             filepath, rollduration, proc)
     client.run()
