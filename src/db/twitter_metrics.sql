d21768764d16b0c43c3bccf1544e32b0473220e1
Scott Hendrickson <shendrickson@gnip.com>
2013-09-03 20:00:43 +0000

    Correct table defs

--- src/db/twitter_metrics.sql
+++ src/db/twitter_metrics.sql
@@ -2,32 +2,27 @@ use stream_twitter_metrics;
 
 DROP TABLE IF EXISTS `language`;
 CREATE TABLE `language` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `language` varchar (10) NOT NULL,
-  `count` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`, `datetime`,`language`),
-  KEY `index_on_lang` (`language`),
-  KEY `index_on_datetime` (`datetime`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+      `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+      `language` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+      `count` int(11) NOT NULL DEFAULT '0',
+      PRIMARY KEY (`datetime`,`language`),
+      KEY `index_on_lang` (`language`),
+      KEY `index_on_time_id` (`datetime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `verb`;
 CREATE TABLE `verb` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `verb` varchar (10) NOT NULL,
-  `count` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`, `datetime`,`verb`),
-  KEY `index_on_verb` (`verb`),
-  KEY `index_on_datetime` (`datetime`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+      `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+      `verb` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+      `count` int(11) NOT NULL DEFAULT '0',
+      PRIMARY KEY (`datetime`,`verb`),
+      KEY `index_on_verb` (`verb`),
+      KEY `index_on_time_id` (`datetime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `counts`;
 CREATE TABLE `counts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `count` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`, `datetime`),
-  KEY `index_on_datetime` (`datetime`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+      `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+      `count` int(11) NOT NULL DEFAULT '0',
+      PRIMARY KEY (`datetime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
