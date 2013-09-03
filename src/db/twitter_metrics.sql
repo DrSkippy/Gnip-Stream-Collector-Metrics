f18fefa5c9f735bf1302c3e08a286f3330d7c5b1
Scott Hendrickson <scott@drskippy.net>
2013-09-03 13:53:42 -0600

    Started testing pubs

--- src/db/twitter_metrics.sql
+++ src/db/twitter_metrics.sql
@@ -2,32 +2,28 @@ use twitter_metrics;
 
 DROP TABLE IF EXISTS `language`;
 CREATE TABLE `language` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `language` varchar (10) NOT NULL,
+  `language` varchar (8) NOT NULL,
   `count` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id, datetime`,`language`),
+  PRIMARY KEY (`datetime`,`language`),
   KEY `index_on_lang` (`language`),
-  KEY `index_on_datetime` (`datetime`)
+  KEY `index_on_time_id` (`datetime`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `verb`;
 CREATE TABLE `verb` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `verb` varchar (10) NOT NULL,
+  `verb` varchar (12) NOT NULL,
   `count` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id, datetime`,`verb`),
+  PRIMARY KEY (`datetime`,`verb`),
   KEY `index_on_verb` (`verb`),
-  KEY `index_on_datetime` (`datetime`)
+  KEY `index_on_time_id` (`datetime`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `counts`;
 CREATE TABLE `counts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `count` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id, `datetime`),
-  KEY `index_on_datetime` (`datetime`)
+  PRIMARY KEY (`datetime`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
